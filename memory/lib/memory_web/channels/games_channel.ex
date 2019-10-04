94005f21a81f8143d43c14482517395bed1ac085
Memory2 <memory2@guest.guest>
2019-10-04 01:47:29 +0000

    modified PORT

--- memory/lib/memory_web/channels/games_channel.ex
+++ memory/lib/memory_web/channels/games_channel.ex
@@ -1,5 +1,7 @@
 defmodule MemoryWeb.GamesChannel do
   use MemoryWeb, :channel
+  alias Memory.Game
+  alias Memory.BackupAgent
 
   def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
@@ -14,17 +16,23 @@ defmodule MemoryWeb.GamesChannel do
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+
+  def handle_in("guess", %{"index" => i}, socket) do
+    name = socket.assigns[:name]
+    game = Game.guess(socket.assigns[:game], i)
+    socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (games:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
+
+
+  def handle_in("restart", payload, socket) do
+    name = socket.assigns[:name]
+    game = Game.new()
+    socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
   # Add authorization logic here as required.
